7901365952f45800896b754cf3fb3393ad6adcfb
Alexandr <jacoby3210@gmail.com>
2024-07-27 16:02:13 +0300

    optimize test page component

--- src/gui/pages/test/index.jsx
+++ src/gui/pages/test/index.jsx
@@ -9,31 +9,51 @@ const Test = receivedProps => {
 	const produceEntries = (count, func) => Array.from(new Array(count), func);
 
 	// basics
-	const indicatorProps = {
-		colors: ["green", "orange", "red"],
-		levels: [100, 60, 25],
-		value: 50
-	};
-	const pathProps = { data: "/test/3", };
-	const popupProps = { shown: true, };
-	const viewProps = {
-		from: 0,
-		length: 10,
-		src: produceEntries(10, (_, i) => { return { text: `string_${i}`}}),
-	};
+	const TestIndicator = () => {
+		const indicatorProps = {
+			colors: ["green", "orange", "red"],
+			levels: [100, 60, 25],
+			value: 50
+		};
+		return (<Common.Indicator {...indicatorProps}/>);
+	}
+	const TestPath = () => {
+		const pathProps = { data: "/test/3", };
+		return <Common.Path {...pathProps}/>;
+	}
+	const TestPopup = () => {
+		const popupProps = { shown: true, };
+		return (<Common.Popup {...popupProps}/>);
+	}
+	const TestView = () => {
+		const viewProps = {
+			from: 0,
+			length: 10,
+			src: produceEntries(10, (_, i) => { return { text: `string_${i}`}}),
+		};
+		return (<Common.View {...viewProps}/>);
+	}
 
 	// dropdown
-	const dropdownProps = {
-		src: produceEntries(5, (v, i) => { return { caption: `Option #${i}`, value: i } }),
-		value: 0
+	const TestDropdown = () => {
+		const dropdownProps = {
+			src: produceEntries(5, (v, i) => { return { caption: `Option #${i}`, value: i } }),
+			value: 0
+		}
+		return (<Common.Dropdown {...dropdownProps} />);
 	}
 
 	// range
 	const rangeHorizontalProps = { axis: true, min: 0, max: 10, step: 0.0001, value: 5 };
 	const rangeVerticalProps = { min: 0, max: 50, step: 0.1, value: 5 };
+	const TestRangeHorizontal = receivedProps => <Common.Range {... rangeHorizontalProps} />;
+	const TestRangeVertical = receivedProps => <Common.Range {... rangeVerticalProps} />;
 
 	// slider
-	const sliderProps = { ...rangeVerticalProps, }
+	const TestSlider = () => {
+		const sliderProps = { ...rangeVerticalProps, }
+		return (<Common.Slider {...sliderProps} />);
+	}
 
 	// advisor
 	const advisorProps = {
@@ -43,93 +63,119 @@ const Test = receivedProps => {
 	// data view and special controllers for mage output data.
 
 	// accordion
-	const accordionProps = {
-		src: produceEntries(5, (v, i) => { return { caption: `Option #${i}`, content: i } }),
+	const TestAccordion = ()=>{
+		const accordionProps = {
+			src: produceEntries(5, (v, i) => { return { caption: `Option #${i}`, content: i } }),
+		}
+		return (<Common.Accordion {...accordionProps} />)
 	}
 
-	// browser controller
-	const [currentBrowseTab, setBrowseTab] = useState(0);
-	const browserProps = {
-		length: 5,
-		src: produceEntries(5, (v, i) => { return { caption: `Option #${i}`, id: i } }),
-		onChangeCallback: (tabIndex) => { setBrowseTab(tabIndex * 10) },
-	}
-	const viewPropsForBrowser = {
-		from: currentBrowseTab,
-		length: 10,
-		src: Array.from({ length: 250 }, (_, i) => { return { text: `string_${i}` } }),
-	}
-
-	// nav controller
-	const [currentNavigatorSlide, setCurrentNavigatorSlide] = useState(0);
-	const navigatorProps = {
-		infinity: true,
-		length: 250,
-		onChangeCallback: (slideIndex) => { setCurrentNavigatorSlide(slideIndex); },
-	}
-	const viewPropsForNavigator = {
-		from: currentNavigatorSlide,
-		length: 1,
-		src: Array.from({ length: 250 }, (_, i) => { return { text: `string_${i}` } }),
+	// browser 
+	const TestBrowser = () => {
+		const [currentBrowseTab, setBrowseTab] = useState(0);
+		const browserProps = {
+			length: 5,
+			src: produceEntries(5, (v, i) => { return { caption: `Option #${i}`, id: i } }),
+			onChangeCallback: (tabIndex) => { setBrowseTab(tabIndex * 10) },
+		}
+		const viewPropsForBrowser = {
+			from: currentBrowseTab,
+			length: 10,
+			src: Array.from({ length: 250 }, (_, i) => { return { text: `string_${i}` } }),
+		}
+		return (
+			<>
+				<Common.Browser {...browserProps} />
+				<Common.View  {...viewPropsForBrowser} />
+			</>
+		)
 	}
 
-	// paginator controller
-	const [currentPageState, setCurrentPageState] = useState(0);
-	const paginatorProps = {
-		lengthBrowser: 5,
-		lengthNavigator: 25,
-		src: produceEntries(25, (v, i) => { return { caption: `Option #${i}`, id: i } }),
-		value: 0,
-		onChangeCallback: (pageIndex) => { setCurrentPageState(pageIndex * 10); },
-	}
-	const viewPropsForPaginator = {
-		from: currentPageState,
-		length: 10,
-		src: Array.from({ length: 250 }, (_, i) => { return { text: `string_${i}` } }),
+	// navigator
+	const TestNavigator = () => {
+		const [currentNavigatorSlide, setCurrentNavigatorSlide] = useState(0);
+		const navigatorProps = {
+			infinity: true,
+			length: 250,
+			onChangeCallback: (slideIndex) => { setCurrentNavigatorSlide(slideIndex); },
+		}
+		const viewPropsForNavigator = {
+			from: currentNavigatorSlide,
+			length: 1,
+			src: Array.from({ length: 250 }, (_, i) => { return { text: `string_${i}` } }),
+		}
+
+		return (
+			<>
+				<Common.Navigator {...navigatorProps} />
+				<Common.View  {...viewPropsForNavigator} />
+			</>
+		);
 	}
 
-	// scrollbar
-	const area = React.useRef(null)
-	const TestAreaForScroll = () =>
-		<div style={{ height: "100px", "overflowY": "scroll" }} ref={area}>
-			<div style={{ height: "200px" }} />
-		</div>
-	const scrollbarProps = { target: area,}
+	// Paginator
+	const TestPaginator = () => {
+		const [currentPageState, setCurrentPageState] = useState(0);
+		const paginatorProps = {
+			lengthBrowser: 5,
+			lengthNavigator: 25,
+			src: produceEntries(25, (v, i) => { return { caption: `Option #${i}`, id: i } }),
+			value: 0,
+			onChangeCallback: (pageIndex) => { setCurrentPageState(pageIndex * 10); },
+		}
+		const viewPropsForPaginator = {
+			from: currentPageState,
+			length: 10,
+			src: Array.from({ length: 250 }, (_, i) => { return { text: `string_${i}` } }),
+		}
 
+		return (
+			<>
+				<Common.Paginator {...paginatorProps} />
+				<Common.View  {...viewPropsForPaginator} />
+			</>
+		);
+	}
+
+	// Scrollbar
+	const TestScrollbar = () => {
+
+		const area = React.useRef(null)
+		const TestAreaForScroll = () =>
+			<div style={{ height: "100px", "overflowY": "scroll" }} ref={area}>
+				<div style={{ height: "200px" }} />
+			</div>
+		const scrollbarProps = { target: area,}
+
+		return (
+			<>
+				<Common.Scrollbar {...scrollbarProps} />
+				<TestAreaForScroll />
+			</>
+		)
+	}
 	
 	// render page
 	return (
 		<>
 			{/* Part I */}
-			<Common.Indicator {...indicatorProps} />
-			<Common.Path {...pathProps} />
-			<Common.Popup {...popupProps} />
-			<Common.View  {...viewProps} />
+			<TestIndicator />
+			<TestPath />
+			<TestPopup />
+			<TestView />
 		
 			{/* Part II */}
-			<Common.Dropdown {...dropdownProps} />
-			<Common.Range {...rangeHorizontalProps} />
-			<Common.Range {...rangeVerticalProps} />
-			<Common.Slider {...sliderProps} />
+			<TestDropdown/>
+			<TestRangeHorizontal />
+			<TestRangeVertical />
+			<TestSlider/>
 			
 			{/* Part III */}
-			<Common.Accordion {...accordionProps} />
-
-			<Common.Browser {...browserProps} />
-			<Common.View  {...viewPropsForBrowser} />
-			
-			<Common.Navigator {...navigatorProps} />
-			<Common.View  {...viewPropsForNavigator} />
-
-			<Common.Paginator {...paginatorProps} />
-			<Common.View  {...viewPropsForPaginator} />
-
-			<Common.Scrollbar {...scrollbarProps} />
-			<TestAreaForScroll />
-
-			{/*
-				<Common.Advisor {...advisorProps} />
-			*/}
+			<TestAccordion/>
+			<TestBrowser/>
+			<TestNavigator/>
+			<TestPaginator/>
+			<TestScrollbar/>
 
 		</>
 	);
